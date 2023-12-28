09fd29d783163aed7e53efb262115a0a34ed9dfd
Maxim Sushkov <max_123_99@mail.ru>
2023-12-28 21:51:50 +1000

    feat: add animation for navigation

--- js/home.js
+++ js/home.js
@@ -2,3 +2,17 @@ document.querySelector(".burger__icon").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".nav__menu").classList.toggle("open");
 });
+
+
+// document.querySelector(".nav__menu").addEventListener("click", function () {
+//   this.classList.toggle("active");
+//   document.querySelector(".nav__menu").classList.toggle("open");
+//   if (this.classList.contains("close")) {
+//     document.querySelector(".nav__menu").classList.remove("close");
+//     document.querySelector(".nav__menu").classList.add("open");
+//   } else {
+//     document.querySelector(".nav__menu").classList.remove("open");
+//     document.querySelector(".nav__menu").classList.add("close");
+//   }
+// });
+//додумайся сам жальше
